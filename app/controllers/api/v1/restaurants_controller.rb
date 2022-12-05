0d808351f7a069ec66101ca2bb9a1737c0b50c94
w-egg <nakajima.kuhi@gmail.com>
2022-12-05 15:10:47 +0900

    Backend Implement

---
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -0,0 +1,15 @@
+module Api
+  module V1
+    class RestaurantsController < ApplicationController
+
+      def index
+        restaurants = Restaurant.all
+
+        render json: {
+          restaurants: restaurants
+        }, status: :ok
+      end
+
+    end
+  end
+end
